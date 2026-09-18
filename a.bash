9e03814ffc15f2195c3c012de72e4d2c65f8cf2e
DarkhanSol0 <darkhansol069@gmail.com>
2026-09-18 18:52:50 +0900

    hhh

--- a.bash
+++ a.bash
@@ -1,5 +1,5 @@
 # Более продвинутая версия для bash
 find . -name "*.md" -type f | sort | while read file; do
-    echo -e "\n\n# 📄 ${file}\n" >> 123.md
-    cat "$file" >> 123.md
+    echo -e "\n\n# 📄 ${file}\n" >> GameCore.md
+    cat "$file" >> GameCore.md
 done
